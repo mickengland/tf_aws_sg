88c4bd41f63b6dc60b2c063480cb95f7576f1000
Mick England <mick@robinpowered.com>
2017-01-03 17:05:10 -0500

    Change variable for cidr blocks to type list

--- sg_web/variables.tf
+++ sg_web/variables.tf
@@ -8,6 +8,7 @@ variable "vpc_id" {
 }
 
 variable "source_cidr_block" {
-  description = "The source CIDR block to allow traffic from"
+  description = "The CIDR blocks to allow traffic from"
+  type = "list"
 }
 
